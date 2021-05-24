fae708dab0c34bd162505274626c651390f7a687
Echo <longshensanjie@gmail.com>
2021-05-24 21:50:51 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
   title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  author: 'Echo',
+  email: 'qin233@qq.com',
+  link: 'https://nobelium.echo.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
   appearance: 'auto', // ['light', 'dark', 'auto'],
